108b6a460e9eb70fc52253bd65bdac21c7e66c6e
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2023-12-25 23:37:00 -0300

    imprimindo listas diferentes com o tipo de lista curinga

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,22 @@
 package application;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		List<?> myObjs = new ArrayList<Object>();
-		List<Integer> myNumbers = new ArrayList<Integer>();
-		myObjs = myNumbers;
-		
-		
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
+		printList(myInts);
+		List<String> myStrs = Arrays.asList("Maria", "Alex", "Bob");
+		printList(myStrs);
+	}
+	// Método
+	public static void printList(List<?> list) {
+		for(Object obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
